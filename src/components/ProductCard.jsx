1b69c8e78ec8083e4d426c26826f04f2af1b4784
Manirajkumar1 <manirajsingh7667@gmail.com>
2025-11-15 12:05:19 +0530

    add search

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,8 +1,11 @@
 import Product from "./Product";
 import { useEffect, useState } from "react";
+import Skeleton from "./Skeleton";
 
 const ProductCard = () => {
   const [productList, setProductList] = useState([])
+  const [tempProductList, setTempProductList] = useState([])
+  const [searchText, setSearchText] = useState("")
 
   useEffect(() => {
     fetchData()
@@ -14,19 +17,40 @@ const ProductCard = () => {
     console.log(resData);
     
     setProductList(resData)
+    setTempProductList(resData)
     
+  }
+
+  // searchtextbtn and serchinput
+
+  const searchInputText = (e) => {
+    setSearchText(e.target.value)
+  }
+ 
+  const searchTextBtn = () => {
+    const filterTextData = productList.filter((product) => {
+      return product.title.includes(searchText.toLowerCase())
+    })
+    setTempProductList(filterTextData)
   }
 
   // filter button
   const topRatedProduct = () => {
     const filteredProduct = productList.filter(product => product.rating.rate >= 4 )
-    setProductList(filteredProduct)
+    setTempProductList(filteredProduct)
+    
   }
-  return (
+
+ 
+  return productList.length === 0 ? <Skeleton/> : (
     <div>
+      <div className="m-2 flex gap-2">
+        <input type="text" onChange={searchInputText} value={searchText}  className="border border-gray-400 "/>
+        <button onClick={searchTextBtn}>Search</button>
+      </div>
       <button onClick={topRatedProduct} className="bg-red-400 text-white m-2 p-2 rounded-xl hover:bg-amber-400 cursor-pointer">Top Rated Product</button>
       <div className="flex w-full flex-wrap">
-        {productList.map((product) => {
+        {tempProductList.map((product) => {
           return <Product key={product.id} product={product} />;
         })}
       </div>
---
+++ src/components/Skeleton.jsx
@@ -0,0 +1,13 @@
+function Skeleton() {
+  return (
+    <div className="border border-gray-300 rounded-lg shadow-sm w-50 hover:shadow-md m-2 p-2 cursor-pointer h-80 animate-pulse">
+      <div className="w-full h-48 bg-gray-200 mb-4 rounded"></div>
+      <div className="h-5 bg-gray-200 rounded w-3/4 mb-2"></div>
+      <div className="h-4 bg-gray-200 rounded w-1/4 mb-2"></div>
+      <div className="h-5 bg-gray-200 rounded w-1/2"></div>
+    </div>
+
+  )
+}
+
+export default Skeleton
